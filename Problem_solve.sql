e995752a4fc1e966813efeea1ce3c0016317fdee
e-unhee <dkfzhd1509@g.skku.edu>
2025-09-20 13:06:28 +0900

    마지막 문제 업로드

--- Problem_solve.sql
+++ Problem_solve.sql
@@ -7,3 +7,14 @@ SELECT DISTINCT salary AS SecondHighestSalary
 FROM Employee
 ORDER BY salary DESC
 LIMIT 1 OFFSET 1;
+
+-- 178. Rank scores
+-- 임시 table, GROUP BY 개념 추가적으로 필요
+
+SELECT S.score,
+  COUNT(S2.SCORE) AS `rank`
+FROM SCORES S,
+     (SELECT DISTINCT SCORE FROM SCORES)  S2
+WHERE S.SCORE <= S2.SCORE 
+GROUP BY S.ID 
+ORDER BY S.SCORE DESC;
